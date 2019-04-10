f20002b7ef5bae82c8314391f34021a9f5cbbba4
ptya <petr.szab@gmail.com>
2019-04-10 21:25:52 +0200

    Initial setup with SASS

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/images/mito-logo.svg';
+import './styles/index.scss';
+import './styles/App.scss';
 
 class App extends Component {
   render() {
