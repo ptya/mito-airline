3a01bf2377484e9958ad0188d2b6769c8d50b678
ptya <petr.szab@gmail.com>
2019-04-19 01:56:36 +0200

    CustomDatePicker

---
+++ src/components/CustomDatePicker.js
@@ -0,0 +1,88 @@
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import DatePicker from "react-datepicker";
+
+import { StationsContext } from "./StationsProvider";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+const CustomDatePicker = props => {
+  const {
+    departureDate,
+    returnDate,
+    setDepartureDate,
+    setReturnDate
+  } = useContext(StationsContext);
+
+  const [startDate, setStartDate] = useState();
+  const { placeholder, type } = props;
+
+  const handleChange = date => {
+    setStartDate(date);
+    switch (type) {
+      case "departure":
+        return setDepartureDate(date);
+      case "return":
+        return setReturnDate(date);
+      default:
+        return;
+    }
+  };
+
+  const getLimits = () => {
+    const today = new Date();
+    switch (type) {
+      case "departure": {
+        let maxDate;
+        if (returnDate) {
+          maxDate = new Date(returnDate);
+          maxDate.setDate(maxDate.getDate() - 1);
+        } else {
+          maxDate = null;
+        }
+        return {
+          minDate: today,
+          maxDate
+        };
+      }
+      case "return": {
+        let minDate;
+        if (departureDate) {
+          minDate = new Date(departureDate);
+          minDate.setDate(minDate.getDate() + 1);
+        } else {
+          minDate = new Date();
+          minDate.setDate(minDate.getDate() + 1);
+        }
+        return {
+          minDate,
+          maxDate: null
+        };
+      }
+      default:
+        return null;
+    }
+  };
+  const limits = getLimits();
+
+  return (
+    <DatePicker
+      selected={startDate}
+      onChange={handleChange}
+      placeholderText={placeholder}
+      minDate={limits.minDate}
+      maxDate={limits.maxDate}
+    />
+  );
+};
+
+CustomDatePicker.propTypes = {
+  placeholder: PropTypes.string,
+  type: PropTypes.string.isRequired
+};
+
+CustomDatePicker.defaultProps = {
+  placeholder: ""
+};
+
+export default CustomDatePicker;
