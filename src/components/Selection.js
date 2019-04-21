5275521da5f8ebed77d4f32af20873c06c44bc7e
ptya <petr.szab@gmail.com>
2019-04-21 21:08:03 +0200

    Timetable with Selection

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { withRouter } from "react-router-dom";
 import CartProvider from "./CartProvider";
 import Cart from "./Cart";
@@ -13,19 +13,40 @@ import "./styles/Selection.scss";
 
 const Selection = props => {
   const { history } = props;
-  const { origin, destination } = useContext(StationsContext);
-  if (!origin || !destination) history.push("/");
+  const {
+    origin,
+    destination,
+    departureDate,
+    setDepartureDate,
+    setReturnDate
+  } = useContext(StationsContext);
+  if (!origin || !destination || !departureDate) {
+    history.push("/");
+    return null;
+  }
+
+  const handleReturn = () => {
+    setDepartureDate();
+    setReturnDate();
+    history.push("/");
+  };
 
   return (
     <>
       <header className="header">
-        <img className="header__logo" src={logo} alt="Mito Airline" />
+        <button
+          className="header__logo"
+          type="button"
+          onClick={() => handleReturn()}
+        >
+          <img className="header__logo" src={logo} alt="Mito Airline" />
+        </button>
         <p className="header__route">
           <span className="header__info">Leaving from</span>
-          Origin
+          {origin.shortName}
         </p>
         <img className="header__arrows" src={arrows} alt="From - to" />
-        <p className="header__route">Destination</p>
+        <p className="header__route">{destination.shortName}</p>
       </header>
       <CartProvider>
         <div className="main">
@@ -36,11 +57,11 @@ const Selection = props => {
           <Cart className="main__cart" />
           <Timetable
             className="main__timetable main__timetable--out"
-            type="setOutbound"
+            type="outbound"
           />
           <Timetable
             className="main__timetable main__timetable--in"
-            type="setInbound"
+            type="inbound"
           />
         </div>
       </CartProvider>
--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,6 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { CartContext } from "./CartProvider";
+import { StationsContext } from "./StationsProvider";
+import { convertDate } from "../utils/convertDate";
+import { formatMoney } from "../utils/formatMoney";
 
 import smallArrow from "../assets/images/arrow-small.svg";
 import mediumArrow from "../assets/images/arrow-medium.svg";
@@ -9,112 +12,231 @@ import chevron from "../assets/images/chevron.svg";
 import "./styles/Timetable.scss";
 
 const Timetable = props => {
-  const { cartDispatch } = useContext(CartContext);
+  const { cartDispatch, cart } = useContext(CartContext);
+  const {
+    origin,
+    destination,
+    departureDate,
+    returnDate,
+    setDestination,
+    setDepartureDate,
+    setReturnDate
+  } = useContext(StationsContext);
 
   const { className, type } = props;
+  const actionType = type === "outbound" ? "setOutbound" : "setInbound";
+
+  const [day, setDay] = useState(
+    type === "outbound" ? departureDate : returnDate
+  );
+  console.table(day);
+  const [prevDay, setPrevDay] = useState(
+    convertDate(new Date(new Date(day.date).setDate(day.day - 1)))
+  );
+  const [nextDay, setNextDay] = useState(
+    convertDate(new Date(new Date(day.date).setDate(day.day + 1)))
+  );
+  const [flights, setFlights] = useState();
+
+  const fetchFlights = async () => {
+    const dep = type === "outbound" ? origin.iata : destination.iata;
+    const arr = type === "outbound" ? destination.iata : origin.iata;
+    const res = await fetch(
+      `https://mock-air.herokuapp.com/search?departureStation=${dep}&arrivalStation=${arr}&date=${
+        day.isoDate
+      }`
+    );
+    console.log(
+      `https://mock-air.herokuapp.com/search?departureStation=${dep}&arrivalStation=${arr}&date=${
+        day.isoDate
+      }`
+    );
+    const data = await res.json();
+    console.table(data);
+    setFlights(data);
+  };
+
+  useEffect(() => {
+    fetchFlights();
+  }, [day]); // only fetch again if day changes
+
+  const handleDayChange = date => {
+    setFlights();
+    setDay(date);
+    setPrevDay(
+      convertDate(new Date(new Date(date.date).setDate(date.day - 1)))
+    );
+    setNextDay(
+      convertDate(new Date(new Date(date.date).setDate(date.day + 1)))
+    );
+    type === "outbound" ? setDepartureDate(date) : setReturnDate(date);
+  };
 
   const blockClass = `${className} timetable`.trim();
+  const today = new Date();
+
+  const isToday =
+    day.day === today.getDate() &&
+    day.month === today.getMonth() &&
+    day.year === today.getFullYear();
+
+  const isPrevAvailable =
+    !isToday &&
+    (departureDate.date.getDate() !== prevDay.day || !departureDate);
+
+  const isNextAvailable =
+    (returnDate && returnDate.date.getDate() !== nextDay.day) || !returnDate;
+
+  const selectedFare =
+    type === "outbound" ? cart.outboundFare : cart.inboundFare;
+
+  const status = type === "outbound" ? "Outbund" : "Inbound";
+  const from = type === "outbound" ? origin.shortName : destination.shortName;
+  const to = type === "outbound" ? destination.shortName : origin.shortName;
 
   return (
     <div className={blockClass}>
       <div className="timetable__header">
         <h2>
-          Outbound
+          {status}
           <span className="timetable__route">
-            Budapest
+            {from}
             <img src={mediumArrow} alt="to" />
-            Barcelona El Prat
+            {to}
           </span>
         </h2>
       </div>
       <div className="timetable__date">
-        <button type="button" className="timetable__step">
-          <img src={chevron} alt="Previous day" />
-          <span>Wed 7 October</span>
-        </button>
-        <h3>Saturday, 3 November 2015</h3>
-        <button className="timetable__step timetable__step--next">
-          <img src={chevron} alt="Next day" />
-          <span>Sat 10 October</span>
-        </button>
+        <div className="timetable__step">
+          {isPrevAvailable && (
+            <button type="button" onClick={() => handleDayChange(prevDay)}>
+              <img src={chevron} alt="Previous day" />
+              <span>{`${prevDay.shortMonth} ${prevDay.day} ${
+                prevDay.fullMonth
+              }`}</span>
+            </button>
+          )}
+        </div>
+        <h3>{`${day.fullWeekday}, ${day.day} ${day.fullMonth} ${day.year}`}</h3>
+        <div className="timetable__step timetable__step--next">
+          {isNextAvailable && (
+            <button type="button" onClick={() => handleDayChange(nextDay)}>
+              <img src={chevron} alt="Next day" />
+              <span>{`${nextDay.shortMonth} ${nextDay.day} ${
+                nextDay.fullMonth
+              }`}</span>
+            </button>
+          )}
+        </div>
       </div>
       <div className="timetable__times">
-        <table className="table">
-          <thead>
-            <tr className="table__row table__row--header">
-              <th />
-              <th>Basic</th>
-              <th>Standard</th>
-              <th>Plus</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="table__row">
-              <td className="table__time">
-                06:02 <img src={smallArrow} alt="to" /> 07:35
-              </td>
-              <td>
-                <button
-                  className="table__btn"
-                  type="button"
-                  onClick={() => cartDispatch({ type, price: 999 })}
-                >
-                  $9.99
-                </button>
-              </td>
-              <td>
-                <button
-                  className="table__btn"
-                  type="button"
-                  onClick={() => cartDispatch({ type, price: 1999 })}
-                >
-                  $19.99
-                </button>
-              </td>
-              <td>
-                <button
-                  className="table__btn"
-                  type="button"
-                  onClick={() => cartDispatch({ type, price: 2999 })}
-                >
-                  $29.99
-                </button>
-              </td>
-            </tr>
-            <tr className="table__row">
-              <td className="table__time">
-                06:02 <img src={smallArrow} alt="to" /> 07:35
-              </td>
-              <td>
-                <button
-                  className="table__btn"
-                  type="button"
-                  onClick={() => cartDispatch({ type, price: 999 })}
-                >
-                  $9.99
-                </button>
-              </td>
-              <td>
-                <button
-                  className="table__btn"
-                  type="button"
-                  onClick={() => cartDispatch({ type, price: 1999 })}
-                >
-                  $19.99
-                </button>
-              </td>
-              <td>
-                <button
-                  className="table__btn"
-                  type="button"
-                  onClick={() => cartDispatch({ type, price: 2999 })}
-                >
-                  $29.99
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <>
+          {flights && flights.length === 0 && (
+            <p className="timetable__no-flights">
+              A kiválasztott napon nincs elérhető járat.
+            </p>
+          )}
+          {flights && flights.length > 0 && (
+            <table className="table">
+              <thead>
+                <tr className="table__row table__row--header">
+                  <th />
+                  <th>Basic</th>
+                  <th>Standard</th>
+                  <th>Plus</th>
+                </tr>
+              </thead>
+              <tbody>
+                {flights.map(flight => {
+                  if (flight.remainingTicket === 0) return null;
+                  const depTime = convertDate(flight.departure);
+                  const isDisabled =
+                    isToday && Date.now() > depTime.date.getTime();
+                  const arrTime = convertDate(flight.arrival);
+                  return (
+                    <tr
+                      key={flight.flightNumber}
+                      className={
+                        isDisabled
+                          ? "table__row table__row--disabled"
+                          : "table__row"
+                      }
+                    >
+                      <td className="table__time">
+                        {depTime.time}
+                        <img src={smallArrow} alt="to" />
+                        {arrTime.time}
+                      </td>
+                      <td>
+                        <button
+                          className={
+                            selectedFare === flight.fares[0].fareSellKey
+                              ? "table__btn table__btn--active"
+                              : "table__btn"
+                          }
+                          type="button"
+                          disabled={isDisabled}
+                          onClick={() =>
+                            cartDispatch({
+                              type: actionType,
+                              price: flight.fares[0].price * 100,
+                              flight,
+                              fare: flight.fares[0].fareSellKey
+                            })
+                          }
+                        >
+                          {formatMoney(flight.fares[0].price * 100)}
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          className={
+                            selectedFare === flight.fares[1].fareSellKey
+                              ? "table__btn table__btn--active"
+                              : "table__btn"
+                          }
+                          type="button"
+                          disabled={isDisabled}
+                          onClick={() =>
+                            cartDispatch({
+                              type: actionType,
+                              price: flight.fares[1].price * 100,
+                              flight,
+                              fare: flight.fares[1].fareSellKey
+                            })
+                          }
+                        >
+                          {formatMoney(flight.fares[1].price * 100)}
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          className={
+                            selectedFare === flight.fares[2].fareSellKey
+                              ? "table__btn table__btn--active"
+                              : "table__btn"
+                          }
+                          type="button"
+                          disabled={isDisabled}
+                          onClick={() =>
+                            cartDispatch({
+                              type: actionType,
+                              price: flight.fares[2].price * 100,
+                              flight,
+                              fare: flight.fares[2].fareSellKey
+                            })
+                          }
+                        >
+                          {formatMoney(flight.fares[2].price * 100)}
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </>
       </div>
     </div>
   );
