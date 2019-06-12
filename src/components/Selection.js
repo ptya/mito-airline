2f2bcf79422ff7652972a835cbc5ae33d73365f8
ptya <petr.szab@gmail.com>
2019-06-12 20:44:33 +0200

    exchanged Header component

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { withRouter } from "react-router-dom";
 
+import Header from "./Header";
 import CartProvider from "./CartProvider";
 import Cart from "./Cart";
 import Timetable from "./Timetable";
@@ -8,49 +9,22 @@ import TimetableSelect from "./TimetableSelect";
 
 import { StationsContext } from "./StationsProvider";
 
-import logo from "../assets/images/mito-logo.svg";
-import arrows from "../assets/images/arrows.svg";
 import plane from "../assets/images/plane.svg";
 
 const Selection = props => {
   const { history } = props;
-  const {
-    origin,
-    destination,
-    departureDate,
-    returnDate,
-    setDepartureDate,
-    setReturnDate
-  } = useContext(StationsContext);
+  const { origin, destination, departureDate, returnDate } = useContext(
+    StationsContext
+  );
 
   if (!origin || !destination || !departureDate) {
     history.push("/");
     return null;
   }
 
-  const handleReturn = () => {
-    setDepartureDate();
-    setReturnDate();
-    history.push("/");
-  };
-
   return (
     <>
-      <header className="header">
-        <button
-          className="header__logo"
-          type="button"
-          onClick={() => handleReturn()}
-        >
-          <img className="header__logo" src={logo} alt="Mito Airline" />
-        </button>
-        <p className="header__route">
-          <span className="header__info">Leaving from</span>
-          {origin.shortName}
-        </p>
-        <img className="header__arrows" src={arrows} alt="From - to" />
-        <p className="header__route">{destination.shortName}</p>
-      </header>
+      <Header />
       <CartProvider>
         <div className="main">
           <h1 className="main__title">
