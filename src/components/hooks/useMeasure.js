ddd49429029e5871b31a99cb84b026a88fd4c835
ptya <petr.szab@gmail.com>
2019-06-19 10:09:01 +0200

    resize observer with hook

---
+++ src/components/hooks/useMeasure.js
@@ -0,0 +1,21 @@
+// https://gist.github.com/stolinski/1fecfb2677e0b2deda9039608fecf5d8
+
+import { useRef, useState, useEffect } from "react";
+import ResizeObserver from "resize-observer-polyfill";
+
+function useMeasure() {
+  const ref = useRef();
+  const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 });
+  const [ro] = useState(
+    () => new ResizeObserver(([entry]) => set(entry.contentRect))
+  );
+  useEffect(() => {
+    ro.observe(ref.current);
+    return () => {
+      ro.disconnect();
+    };
+  }, [ro]);
+  return [{ ref }, bounds];
+}
+
+export { useMeasure };
