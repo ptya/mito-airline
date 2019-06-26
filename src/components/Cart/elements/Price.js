7c51df8ed0c4aa2448c4b12b9cff456b71829618
ptya <petr.szab@gmail.com>
2019-06-26 22:41:26 +0200

    Price component for animated price

---
+++ src/components/Cart/elements/Price.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { animated, useSpring } from "react-spring";
+
+const Price = props => {
+  const animation = useSpring({
+    price: props.price
+  });
+
+  return (
+    <animated.span>
+      {animation.price.interpolate(x => `$${(x / 100).toFixed(0)}`)}
+    </animated.span>
+  );
+};
+
+Price.propTypes = {
+  price: PropTypes.number.isRequired
+};
+
+export default Price;
