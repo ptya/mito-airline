53462cd25ccfe8719562f51a1212982824bf0c38
ptya <petr.szab@gmail.com>
2019-06-30 11:35:43 +0200

    animated Switch for destination switch

---
+++ src/components/Selection/Header/elements/Switch.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { animated, useSpring } from "react-spring";
+
+import { colors } from "components/styles/variables";
+
+// assets
+import arrows from "assets/images/arrows.svg";
+
+const Button = styled(animated.button)`
+  border: 0;
+  background-color: initial;
+  cursor: pointer;
+
+  img {
+    width: 30px;
+    margin: auto 0;
+  }
+`;
+
+const Switch = () => {
+  const [state, toggle] = useState(true);
+  const { z } = useSpring({
+    to: {
+      z: state ? 0 : 180
+    }
+  });
+
+  return (
+    <Button
+      style={{
+        transform: z.interpolate(z => `rotateZ(${z}deg)`)
+      }}
+      onClick={() => toggle(!state)}
+    >
+      <img src={arrows} alt="From - to" />
+    </Button>
+  );
+};
+
+export default Switch;
