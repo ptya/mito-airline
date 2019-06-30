0d7d141dbb916544b59b62fa999dc190fb4fbb62
ptya <petr.szab@gmail.com>
2019-06-30 17:03:16 +0200

    removed unused

--- src/components/Cart/elements/AnimatedFlight.js
+++ src/components/Cart/elements/AnimatedFlight.js
@@ -8,18 +8,12 @@ const Wrapper = styled(animated.div)`
   background: ${colors.white};
 `;
 
-// transform rotateX -90 -> 0
-// translateY -38 -> 0
-
 const AnimatedFlight = props => {
   // animation related
-  const { x, y, xy } = useSpring({
-    from: { x: -90, y: -38, xy: [90, 38] },
-    x: 0,
-    y: 0,
+  const { xy } = useSpring({
+    from: { xy: [90, 38] },
     xy: [0, 0],
     config: config.gentle
-    // skew or something
   });
 
   return (
@@ -28,10 +22,6 @@ const AnimatedFlight = props => {
         transform: xy.interpolate(
           (x, y) => `translateY(-${y}px) rotateX(-${x}deg)`
         )
-        // transform: interpolate(
-        //   [x, y],
-        //   (x, y) => `rotateX(${x}) translateY(${y})`
-        // )
       }}
     >
       {props.children}
