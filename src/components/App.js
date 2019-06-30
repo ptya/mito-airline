8bd895947331347bbed23689c9a59eb42f4ffb74
ptya <petr.szab@gmail.com>
2019-06-30 17:18:49 +0200

    removed the last bit of css

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import Routes from "./Routes";
 import GlobalStyle from "./styles/GlobalStyle";
 
-import "./styles/index.scss";
-
 const App = () => {
   return (
     <>
