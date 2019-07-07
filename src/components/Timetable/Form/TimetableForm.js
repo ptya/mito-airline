d87e0d2572a50fd03b29899f41dbfd81f0239f8d
ptya <petr.szab@gmail.com>
2019-07-07 16:35:51 +0200

    missing error handling added

--- src/components/Timetable/Form/TimetableForm.js
+++ src/components/Timetable/Form/TimetableForm.js
@@ -1,10 +1,18 @@
-import React, { useContext } from "react";
+import React, {
+  useContext,
+  useCallback,
+  useState,
+  useLayoutEffect
+} from "react";
 
 // components
 import CustomDatePicker from "components/CustomDatePicker";
 
 // context
 import { StationsContext } from "components/providers/StationsProvider";
+
+// foreign elements
+import Error from "components/Home/elements/Error";
 
 // global styles
 import CalendarIco from "components/styles/CalendarIco";
@@ -20,19 +28,43 @@ const TimetableForm = () => {
     stations: { secondaryReturnDate }
   } = useContext(StationsContext);
 
+  const [error, setError] = useState();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const validate = useCallback(() => {
+    const e = {};
+    if (!secondaryReturnDate) e.secondaryReturnDate = "Please select departure";
+    setError(e);
+    return e.secondaryReturnDate ? true : false;
+  }, [secondaryReturnDate]);
+
   const onSubmit = event => {
     event.preventDefault();
-    if (secondaryReturnDate) {
+    const err = validate();
+    if (!isSubmitted) setIsSubmitted(true);
+
+    if (!err) {
       stationsDispatch({ type: "setReturn", returnDate: secondaryReturnDate });
     }
   };
 
+  useLayoutEffect(() => {
+    // skip validation until first submit
+    if (!isSubmitted) {
+      return;
+    }
+    validate();
+  }, [secondaryReturnDate, isSubmitted, validate]);
+
   return (
     <Form autoComplete="off" onSubmit={onSubmit}>
       <fieldset>
-        <InputField>
+        <InputField isError={error && error.secondaryReturnDate}>
           <CustomDatePicker placeholder="Return" type="secondaryReturn" />
           <CalendarIco />
+          {error && error.secondaryReturnDate && (
+            <Error message={error.secondaryReturnDate} />
+          )}
         </InputField>
         <SearchButton type="submit">Search</SearchButton>
       </fieldset>
