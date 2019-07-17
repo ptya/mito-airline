958650886dbd4a276e27b1e13f264b3cb982bd13
ptya <petr.szab@gmail.com>
2019-07-17 14:55:22 +0200

    add jest-styled-components

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,5 @@
 import "@testing-library/react/cleanup-after-each";
 import "@testing-library/jest-dom/extend-expect";
+import "jest-styled-components";
 require("jest-localstorage-mock");
 global.fetch = require("jest-fetch-mock");
